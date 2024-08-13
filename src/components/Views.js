aa4140d831db903c14e795f969a05f56bca5e7e6
SharmaRavin <ravinder.acs@gmail.com>
2024-08-14 01:49:01 +0530

    customize formik componant

--- src/components/Views.js
+++ src/components/Views.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 const Views = () => {
   return (
