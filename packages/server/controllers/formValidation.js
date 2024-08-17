0c7ed6fb7fc166cb17d2abbaabe13d9301460818
SharmaRavin <ravinder.acs@gmail.com>
2024-08-17 22:59:58 +0530

    adding authentication middleware

--- packages/server/controllers/formValidation.js
+++ packages/server/controllers/formValidation.js
@@ -1,17 +1,24 @@
 const { formSchema } = require("@rtcom-app/common");
 
-const validateFrom = (req, res) => {
+const validateFrom = (req, res, next) => {
   const formData = req.body;
   try {
     formSchema
       .validate(formData)
       .catch((err) => {
         res.status(422).send();
-        console.log("err---", err.errors);
+        console.log("validation error", err.errors);
       })
-      .then((res) => console.log("response.....", res));
+      .then((res) => {
+        if (res) {
+          console.log("successfull");
+          next();
+        } else {
+          res.status(422).send();
+        }
+      });
   } catch (err) {
-    console.log("try catch eerro", err);
+    console.log("validation error catch", err);
   }
 };
 
